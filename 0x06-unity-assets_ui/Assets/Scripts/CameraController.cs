bd7bce088c86a50e42187f2484de1cee9ef336bb
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-09-21 19:56:34 +0300

    add isInverted

--- 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -7,15 +7,19 @@ public class CameraController : MonoBehaviour
     public GameObject player;
     private Vector3 offset;
     private float turnSpeed = 3f;
+    public bool isInverted;
     void Start()
     {
         offset = new Vector3(0f, 1.5f, -7f);
+        if(PlayerPrefs.GetString("isInverted") == "False")
+            isInverted = true;
+        else isInverted = false;
     }
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
         offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up * Time.deltaTime) * offset;
-        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * -turnSpeed, Vector3.right * Time.deltaTime) * offset;
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * -turnSpeed, Vector3.right * Time.deltaTime* (isInverted ? -1 : 1)) * offset;
         transform.position = player.transform.position + offset;
         transform.LookAt(player.transform.position);
     }

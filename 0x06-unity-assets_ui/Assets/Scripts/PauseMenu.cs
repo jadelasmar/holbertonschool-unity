041cbb421a3e64aad70ce9a3cc8949b786cd71d8
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-09-21 12:52:43 +0300

    pausecanvas

--- 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject PauseCanvas;
+    public GameObject Camera;
+    public bool gamePaused = false;
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused == true)
+                Resume();
+            else
+                Pause();
+        }
+    }
+    public void Pause()
+    {
+        PauseCanvas.SetActive(true);
+        Camera.SetActive(false);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+    public void Resume()
+    {
+        PauseCanvas.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
     }
 }

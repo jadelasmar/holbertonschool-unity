b3d67524612b51ffcc86e453a89ad23bee60fbc1
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-10-01 18:55:29 +0300

    fix timer start afer animation

--- 0x07-unity-animation/Assets/Scripts/TimerTrigger.cs
+++ 0x07-unity-animation/Assets/Scripts/TimerTrigger.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class TimerTrigger : MonoBehaviour
 {
+
+    private PlayerController playerController;
+    void Start()
+    {
+        playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+    }
     void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && playerController.enabled == true)
         {
             other.gameObject.GetComponent<Timer>().enabled = true;
         }

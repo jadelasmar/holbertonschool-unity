053a92f6dd83a1beea677e71ed50c598084c3508
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-10-01 20:15:02 +0300

    new controller //justinmajetich

--- 0x07-unity-animation/Assets/Scripts/Timer.cs
+++ 0x07-unity-animation/Assets/Scripts/Timer.cs
@@ -7,6 +7,8 @@ public class Timer : MonoBehaviour
 {
     public Text TimerText;
     public float millisec, sec, min, iTimer;
+
+    private bool timerIsRunning = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +18,17 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        iTimer += Time.deltaTime;
-        millisec = (int)(iTimer * 100) % 100;
-        sec = (int)(iTimer % 60f);
-        min = (int)(iTimer / 60f);
-        TimerText.text = String.Format("{0:0}:{1:00}:{2:00}",min,sec,millisec);
-
+        if (timerIsRunning)
+        {
+            iTimer += Time.deltaTime;
+            millisec = (int)(iTimer * 100) % 100;
+            sec = (int)(iTimer % 60f);
+            min = (int)(iTimer / 60f);
+            TimerText.text = String.Format("{0:0}:{1:00}:{2:00}", min, sec, millisec);
+        }
+    }
+    public void Run()
+    {
+        timerIsRunning = true;
     }
 }
--- 0x07-unity-animation/Assets/Scripts/TimerTrigger.cs
+++ 0x07-unity-animation/Assets/Scripts/TimerTrigger.cs
@@ -2,17 +2,24 @@ using UnityEngine;
 
 public class TimerTrigger : MonoBehaviour
 {
-
+    private Timer timer;
     private PlayerController playerController;
+
     void Start()
     {
+        timer = GameObject.FindWithTag("Player").GetComponent<Timer>();
         playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
     }
+
     void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player" && playerController.enabled == true)
-        {
-            other.gameObject.GetComponent<Timer>().enabled = true;
-        }
+            EnableTimer();
+    }
+
+    private void EnableTimer()
+    {
+        timer.enabled = true;
+        timer.Run();
     }
 }

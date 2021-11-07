088783025d0e73863e7b84b5ed4e6a24a1fa3068
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-11-07 19:36:46 +0200

    Level01 BGM

--- 0x08-unity-audio/Assets/Scripts/MusicScript.cs
+++ 0x08-unity-audio/Assets/Scripts/MusicScript.cs
@@ -8,6 +8,7 @@ public class MusicScript : MonoBehaviour
     public AudioClip bgm;
 
     public AudioSource audioSource;
+    public AudioMixer audioMixer;
 
     // Start is called before the first frame update
     void Start()
